9af4dd94b6f2364857159a74d3cd4b1d05abb774
Nick Qi <NickWei.Qi@accordo.com>
2018-11-20 11:06:21 +1300

    Updates

---
+++ precache-manifest.562e37d0c747aa6f2252b5a009add012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b2d1a7842549eda0e255e67f179290",
+    "url": "/demo/static/media/bus-top.c2b2d1a7.png"
+  },
+  {
+    "revision": "df776aaf92209c815866",
+    "url": "/demo/static/js/runtime~main.df776aaf.js"
+  },
+  {
+    "revision": "d466ae08a429a147a31a",
+    "url": "/demo/static/js/main.d466ae08.chunk.js"
+  },
+  {
+    "revision": "3f1e5167d28184eb03a9",
+    "url": "/demo/static/js/1.3f1e5167.chunk.js"
+  },
+  {
+    "revision": "d466ae08a429a147a31a",
+    "url": "/demo/static/css/main.3103d567.chunk.css"
+  },
+  {
+    "revision": "c04ab8e2d8a68158a1238ceeed83f079",
+    "url": "/demo/index.html"
+  }
+];
